2421a901cbcec7d9c9fcdac6005a455e3aa75eb1
Alexander Zielenski <alex@alexzielenski.com>
2015-06-10 17:37:21 -0700

    give the uti tool more functionality

--- uti/main.m
+++ uti/main.m
@@ -10,8 +10,27 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSString *uti = (__bridge_transfer NSString *)(UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, (__bridge CFStringRef)(@(argv[1])), NULL));
-        printf("%s\n", uti.UTF8String);
+        if (argc <= 1 || argc > 3) {
+            printf("Usage: uti <extension> or uti -e <uti>\n");
+            return 1;
+        } else if (argc == 2) {
+            NSString *uti = (__bridge_transfer NSString *)(UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, (__bridge CFStringRef)(@(argv[1])), NULL));
+            printf("%s\n", uti.UTF8String);
+        } else if (argc == 3) {
+            CFStringRef class = NULL;
+            if (strcmp(argv[1], "-e") == 0) {
+                class = kUTTagClassFilenameExtension;
+            } else if (strcmp(argv[1], "-o") == 0) {
+                class = kUTTagClassOSType;
+            } else if (strcmp(argv[1], "-m") == 0) {
+                class = kUTTagClassMIMEType;
+            } else {
+                class = kUTTagClassFilenameExtension;
+            }
+            
+            NSArray *types = (__bridge_transfer NSArray *)UTTypeCopyAllTagsWithClass((__bridge CFStringRef)@(argv[2]), class);
+            printf("%s\n", types.description.UTF8String);
+        }
     }
     return 0;
 }
